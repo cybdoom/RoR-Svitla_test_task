3a91de1add18776df0bf454705e5dc95a3eb5bcb
cybdoom <cybdoom@gmail.com>
2014-05-23 15:36:41 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SvitlaTaskV3::Application.config.secret_key_base = '5dd3556bf881c9acd6b966b6f42140bd26aac6f70c980c9309d08cc07d4c86c09ed5cd5af7456c2c2c52ce04332c6b9fe7de6b5ef87672895826e805857f3035'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SvitlaTaskV3::Application.config.session_store :cookie_store, key: '_svitla_task_v3_session'
